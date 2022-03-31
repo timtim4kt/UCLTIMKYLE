0b26805c58e0aebc3503f13dfd42d3ca7b8fdc9d
kylem <kyle.mcquillan.01@gmail.com>
2022-03-31 13:47:39 +0000

    Added New Image to HomePage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                <h1 class="sm:text-red-800  text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Into The Last 8
                 </h1>
                 <a 
                     href="/blog"
